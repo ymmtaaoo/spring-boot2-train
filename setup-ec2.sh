f7621a5dcb64d38639c16e752889164b19b4dfcc
namickey <teethdai@gmail.com>
2023-07-08 10:24:57 +0900

    mod

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd /home/ec2-user
-
-touch a.txt
-
 # JAVA_HOME
 export JAVA_HOME="/usr/lib/jvm/java-11-amazon-corretto.x86_64/"
 
 # move
 cd /home/ec2-user/spring-boot2-train
 
+chmod 755 mvnw
+
 # spring-boot:run
-nohup /bin/bash mvnw spring-boot:run &
+nohup ./mvnw spring-boot:run &
